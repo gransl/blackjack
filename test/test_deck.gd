6e654916a711250188ddbeda601646734b60deeb
Sandra <gran.sl@gmail.com>
2026-08-02 17:47:08 -0700

    Add tests for Card, Deck, Hand

---
+++ test/test_deck.gd
@@ -0,0 +1,23 @@
+extends GdUnitTestSuite
+
+var test_deck: Deck
+
+func before_test() -> void:
+	test_deck = Deck.new()
+	
+func test_init_creates_deck() -> void:
+	assert_object(test_deck.cards).is_not_null()
+
+func test_init_creates_52_card_deck() -> void:
+	assert_array(test_deck.cards).has_size(52)
+	
+func test_deal_card() -> void:
+	var test_dealt_card: Card = test_deck.deal_card()
+	assert_object(test_dealt_card).is_not_null()
+	assert_array(test_deck.cards).has_size(51)
+
+func test_is_empty_method() -> void:
+	assert_bool(test_deck.is_empty()).is_false()
+	for i in range(52):
+		test_deck.deal_card()
+	assert_bool(test_deck.is_empty()).is_true()
